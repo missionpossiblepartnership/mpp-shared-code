a1e19f6ea5d0f1dced2cd2e318755c643ad2ea26
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-02-09 15:05:23 +0000

    Changed file reference

--- setup.py
+++ setup.py
@@ -15,11 +15,11 @@ else:
 
 # Basic setup information of the library
 setup(
-    name="MPP Shared",
+    name="MPP Shared Code",
     version="0.1",
     description="Library of shared code to support MPP STS Models",
     author="SYSTEMIQ",
-    packages=["MPP Shared"],
+    packages=["mppshared"],
     python_requires=">=3.9",
     install_requires=INSTALL_REQUIRES,
 )
