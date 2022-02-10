545fe9232313672d321046cf8ac99ba43e06682b
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-02-10 16:55:00 +0000

    hotfix test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,5 +6,5 @@ import pandas as pd
 
 def test_move_cols_to_front():
     test_df = pd.DataFrame([(1, 2, 3), (4, 5, 6)], columns=["A", "B", "C"])
-    test_df_mod = move_cols_to_front(test_df, ['C'])
-    assert test_df_mod == ['C', 'A', 'B']
+    mod_cols = move_cols_to_front(test_df, ['C'])
+    assert mod_cols == ['C', 'B', 'A']
