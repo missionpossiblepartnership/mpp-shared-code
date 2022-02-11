a7bcca5c448a521961e9d49987e475c6fc88a71a
drew-sys <90053966+drew-sys@users.noreply.github.com>
2022-02-11 13:45:13 +0000

    updated version of module

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
 # Basic setup information of the library
 setup(
     name="MPP Shared Code",
-    version="0.1",
+    version="0.1.1",
     description="Library of shared code to support MPP STS Models",
     author="SYSTEMIQ",
     packages=["mppshared"],
