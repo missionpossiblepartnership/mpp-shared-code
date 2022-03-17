7758f98aa9f5b4002a3f55aa6370c8dbcde192b5
Johannes Wüllenweber <98021846+JohannesWuellenweberSIQ@users.noreply.github.com>
2022-03-17 18:36:25 +0100

    Script architecture for solver

---
+++ mppshared/solver/solve.py
@@ -0,0 +1,12 @@
+""" Execute the solver."""
+
+# MAIN FUNCTION TO EXECUTE SOLVER
+
+# Validate input tables
+# Apply implicit forcing
+# Create ranking
+# Find optimum technology switches year-by-year
+# Process outputs
+
+# Import carbon budget
+# Grid search
