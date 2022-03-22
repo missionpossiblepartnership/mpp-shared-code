d50c57d351b120e9cd6393cf120a636fa8c54e5a
Johannes Wüllenweber <98021846+JohannesWuellenweberSIQ@users.noreply.github.com>
2022-03-22 09:36:25 +0100

    Update solve.py

--- mppshared/solver/solve.py
+++ mppshared/solver/solve.py
@@ -1,6 +1,6 @@
 """ Execute the solver."""
 
-from mppshared.solver.implicit_forcing import apply_implicit_forcing
+from mppshared.solver.implicit_forcing import apply_implicit_forcing, 
 from mppshared.solver.input_loading import load_and_validate_inputs
 from mppshared.config import SOLVER_INPUT_DATA_PATH
 
@@ -14,11 +14,13 @@ def solve(sector: str):
 
     # Apply implicit forcing
     # TODO: implement caching or optional load from .csv
+    # TODO: implement calculation of emission deltas
     df_ranking = apply_implicit_forcing(
         df_technology_switches=input_dfs["technology_switches"],
         df_emissions=input_dfs["emissions"],
         df_technology_characteristics=input_dfs["technology_characteristics"],
     )
+
 
     pass
     # Output of this should be a technology switching table with cost, emissions and characteristics
