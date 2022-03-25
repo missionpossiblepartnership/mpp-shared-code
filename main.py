974fda74967013d1c8aa594bf4e9fc0a521ae3ef
Luis Natera <natera@hey.com>
2022-03-25 15:27:23 +0100

    Only run ranking for developing

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logger.setLevel(LOG_LEVEL)
 np.random.seed(100)
 
 funcs = {
-    "APPLY_IMPLICIT_FORCING": apply_implicit_forcing,
+    # "APPLY_IMPLICIT_FORCING": apply_implicit_forcing,
     "MAKE_RANKINGS": make_rankings,
     # "SIMULATE_PATHWAY": simulate_pathway,
     # "CALCULATE_OUTPUTS": calculate_outputs,
