7a9a625acb4e2023e6b7490ce7331f31bdcb7a71
Johannes Wüllenweber <98021846+JohannesWuellenweberSIQ@users.noreply.github.com>
2022-03-29 13:58:26 +0200

    Add all scripts to __init__.py

--- mppshared/solver/__init__.py
+++ mppshared/solver/__init__.py
@@ -1,10 +1,1 @@
-from ..models import constraints
-from . import (
-    grid_search,
-    implicit_forcing,
-    input_loading,
-    output_processing,
-    ranking,
-    solve,
-    switching,
-)
+from . import grid_search, implicit_forcing, input_loading, output_processing, ranking
