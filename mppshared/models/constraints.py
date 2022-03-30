263e10680615011bee44de3681a2c9ed707059f4
Johannes Wüllenweber <98021846+JohannesWuellenweberSIQ@users.noreply.github.com>
2022-03-30 16:07:57 +0200

    Add dummy function for constraints check

--- mppshared/models/constraints.py
+++ mppshared/models/constraints.py
@@ -1,1 +1,25 @@
 """ Enforce constraints in the yearly optimization of technology switches."""
+
+from pandera import Bool
+from mppshared.models.plant import Plant, PlantStack
+
+
+def check_constraints(stack: PlantStack) -> Bool:
+    """Check all constraints for a given asset stack.
+
+    Args:
+        stack: stack of assets for which constraints are to be checked
+
+    Returns:
+        Returns True if no constraint hurt
+    """
+    # TODO: improve runtime by not applying all constraints to every agent logic
+
+    # TODO: Check regional production constraint
+
+    # TODO: Check technology ramp-up constraint
+
+    # TODO: Check resource availability constraint
+
+    #! Placeholder
+    return True
