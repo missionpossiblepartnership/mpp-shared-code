58b6cb1c28a3ef949d50c7e48037233d9ac5c3b4
Luis Natera <natera@hey.com>
2022-04-01 09:55:23 +0200

    Data processing script

--- mppshared/outputs/data_processing.py
+++ mppshared/outputs/data_processing.py
@@ -3,7 +3,22 @@
 import pandas as pd
 
 
-def create_outputs_interface():
+def create_table_sequence_technology():
+    pass
+
+
+def create_table_all_data():
+    # table has to be structured as follows:
+    # Columns:
+    # - sector
+    # - product
+    # - technology
+    # - parameter
+    # - unit
+    # - value
+    # - 2020
+    # - nth year
+    #  To fill it can be done with a long table with a year column and then create a pivot table with the years as index
     pass
 
 
