923f152997a4bc5014c596d5427d3daed5277e3c
Luis Natera <natera@hey.com>
2022-04-06 14:32:16 +0200

    Calculations for emissions and CO2 captured

--- mppshared/solver/output_processing.py
+++ mppshared/solver/output_processing.py
@@ -24,6 +24,7 @@ def create_table_all_data_year(year, importer):
     df_stack_total_assets.rename(columns={"asset": "value"}, inplace=True)
     df_stack_total_assets["parameter"] = "Number of plants"
     df_stack_total_assets["unit"] = "plant"
+    df_stack_total_assets["parameter_group"] = "Production"
     # Calculate the production volume per region, product and technology
     df_stack_production_capacity = (
         df_stack.groupby(["product", "region", "technology"]).sum().reset_index()
@@ -33,9 +34,51 @@ def create_table_all_data_year(year, importer):
     )
     df_stack_production_capacity["parameter"] = "Annual production volume"
     df_stack_production_capacity["unit"] = "Mt"
-    # Join the dataframes into a long table
-    df_stack_plants = pd.concat([df_stack_total_assets, df_stack_production_capacity])
-    return df_stack_plants
+    df_stack_production_capacity["parameter_group"] = "Production"
+
+    # Calculate the carbon emissions per region, product and technology
+    df_emissions = importer.get_emissions()
+    df_emissions = df_emissions[df_emissions["year"] == year]
+    df_assets_emissions = df_stack.merge(
+        df_emissions, on=["product", "region", "technology"]
+    )
+    df_stack_emissions = (
+        df_assets_emissions.groupby(["product", "region", "technology"])
+        .sum()
+        .reset_index()
+    )
+    df_stack_emissions_emitted = df_stack_emissions.melt(
+        id_vars=["product", "region", "technology"],
+        value_vars=[
+            "co2_scope1",
+            "co2_scope2",
+            "co2_scope3_upstream",
+            "co2_scope3_downstream",
+        ],
+        var_name="parameter",
+        value_name="value",
+    )
+    df_stack_emissions_emitted["unit"] = "t"
+    df_stack_emissions_emitted["parameter_group"] = "Emissions"
+    df_stack_capture_emissions = df_stack_emissions.melt(
+        id_vars=["product", "region", "technology"],
+        value_vars=[
+            "co2_scope1_captured",
+        ],
+        var_name="parameter",
+        value_name="value",
+    )
+    df_stack_capture_emissions["unit"] = "t"
+    df_stack_capture_emissions["parameter_group"] = "Emissions"
+
+    return pd.concat(
+        [
+            df_stack_total_assets,
+            df_stack_production_capacity,
+            df_stack_emissions_emitted,
+            df_stack_capture_emissions,
+        ]
+    )
 
 
 def calculate_outputs(pathway, sensitivity, sector):
@@ -55,7 +98,15 @@ def calculate_outputs(pathway, sensitivity, sector):
     df["sector"] = sector
     # Pivot the dataframe to have the years as columns
     df_pivot = df.pivot_table(
-        index=["sector", "product", "region", "technology", "parameter", "unit"],
+        index=[
+            "sector",
+            "product",
+            "region",
+            "technology",
+            "parameter_group",
+            "parameter",
+            "unit",
+        ],
         columns="year",
         values="value",
     )
