ceb7b6377d476a7fb061a098a108a26ba869c962
Luis Natera <natera@hey.com>
2022-04-06 11:38:53 +0200

    Logger for outputs processing

--- mppshared/solver/output_processing.py
+++ mppshared/solver/output_processing.py
@@ -1,12 +1,26 @@
 """ Process outputs to standardised output table."""
 import pandas as pd
 
+from mppshared.config import (END_YEAR, LOG_LEVEL, PRODUCTS, SECTOR,
+                              SECTORAL_CARBON_BUDGETS, START_YEAR)
+from mppshared.utility.log_utility import get_logger
+
+logger = get_logger(__name__)
+logger.setLevel(LOG_LEVEL)
+
 
 def create_table_sequence_technology():
     pass
 
 
+def create_table_all_data_year(year):
+
+    pass
+
+
 def create_table_all_data():
+    for year in range(START_YEAR, END_YEAR + 1):
+        create_table_all_data_year(year)
     # table has to be structured as follows:
     # Columns:
     # - sector
