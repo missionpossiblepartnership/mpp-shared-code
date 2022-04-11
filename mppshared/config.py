a5be92517857e25f042fa2092be652dddb67be63
Luis Natera <natera@hey.com>
2022-04-11 13:44:21 +0200

    Regional production share for aluminium and config for aluminium

--- mppshared/config.py
+++ mppshared/config.py
@@ -169,8 +169,8 @@ SENSITIVITIES = [
 
 ### SECTOR-SPECIFIC PARAMETERS ###
 # Sectors for which the model can be run
-SECTOR = "chemicals"
-# SECTOR = "aluminium"
+# SECTOR = "chemicals"
+SECTOR = "aluminium"
 
 # Products produced by each sector
 PRODUCTS = {
@@ -286,19 +286,37 @@ RANKING_CONFIG = {
 INVESTMENT_CYCLE = 20  # years
 
 # TODO: placeholder for external input
-REGIONAL_PRODUCTION_SHARE = {
-    "Africa": 0.3,
-    "China": 0.3,
-    "Europe": 0.3,
-    "India": 0.3,
-    "Latin America": 0.3,
-    "Middle East": 0.3,
-    "North America": 0.3,
-    "Oceania": 0.3,
-    "Russia": 0.3,
-    "Rest of Asia": 0.3,
-}
+# REGIONAL_PRODUCTION_SHARE Ammonia
+# REGIONAL_PRODUCTION_SHARE = {
+#     "Africa": 0.3,
+#     "China": 0.3,
+#     "Europe": 0.3,
+#     "India": 0.3,
+#     "Latin America": 0.3,
+#     "Middle East": 0.3,
+#     "North America": 0.3,
+#     "Oceania": 0.3,
+#     "Russia": 0.3,
+#     "Rest of Asia": 0.3,
+# }
 
+# REGIONAL_PRODUCTION_SHARE Aluminium
+REGIONAL_PRODUCTION_SHARE = {
+    "China - North": 0.3,
+    "China - North West": 0.3,
+    "China - North East": 0.3,
+    "China - Central": 0.3,
+    "China - South": 0.3,
+    "China - East": 0.3,
+    "Rest of Asia": 0.3,
+    "North America": 0.3,
+    "Russia": 0.3,
+    "Europe": 0.3,
+    "Middle East": 0.3,
+    "Africa": 0.3,
+    "South America": 0.3,
+    "Oceania": 0.3,
+}
 # Sectoral carbon budget (scope 1 and 2 CO2 emissions, in GtCO2)
 # TODO: import from .csv file
 SECTORAL_CARBON_BUDGETS = {
