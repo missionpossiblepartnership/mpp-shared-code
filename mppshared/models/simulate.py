53acd3d9ebdbc245b1cbc7523a4e8be4342b4c24
Johannes Wüllenweber <98021846+JohannesWuellenweberSIQ@users.noreply.github.com>
2022-06-22 14:23:15 +0200

    Brownfield needs to come before greenfield

--- mppshared/models/simulate.py
+++ mppshared/models/simulate.py
@@ -45,7 +45,7 @@ def simulate(pathway: SimulationPathway) -> SimulationPathway:
     for year in range(START_YEAR, END_YEAR + 1):
         logger.info("Optimizing for %s", year)
 
-        # Adjust capacity utilisation of each asset
+        # Adjust capacity utilisation of each asset in this year'stack
         pathway = adjust_capacity_utilisation(pathway=pathway, year=year)
 
         # Copy over this year's stack to the next year (for modification by decommission, greenfield and brownfield transitions)
@@ -62,20 +62,20 @@ def simulate(pathway: SimulationPathway) -> SimulationPathway:
             f"Time elapsed for decommission in year {year}: {timedelta(seconds=end-start)} seconds"
         )
 
-        # Build new assets
-        start = timer()
-        pathway = greenfield(pathway=pathway, year=year)
-        end = timer()
-        logger.debug(
-            f"Time elapsed for greenfield in year {year}: {timedelta(seconds=end-start)} seconds"
-        )
-
         # Renovate and rebuild assets (brownfield transition)
         start = timer()
         pathway = brownfield(pathway=pathway, year=year)
         end = timer()
         logger.debug(
             f"Time elapsed for brownfield in year {year}: {timedelta(seconds=end-start)} seconds"
+        )
+
+        # Build new assets
+        start = timer()
+        pathway = greenfield(pathway=pathway, year=year)
+        end = timer()
+        logger.debug(
+            f"Time elapsed for greenfield in year {year}: {timedelta(seconds=end-start)} seconds"
         )
 
     return pathway
