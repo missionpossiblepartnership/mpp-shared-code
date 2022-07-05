1de81873b4e6794cf071d025ab775004405f6fb7
Johannes Wüllenweber <98021846+JohannesWuellenweberSIQ@users.noreply.github.com>
2022-07-05 15:49:17 +0200

    Update NPV calculations from ammonia_carbon_price branch

--- mppshared/calculate/calculate_cost.py
+++ mppshared/calculate/calculate_cost.py
@@ -11,34 +11,28 @@ logger = get_logger("Cost calculations")
 
 def discount_costs(df_cost: pd.DataFrame, grouping_cols: list) -> pd.DataFrame:
     """Calculate NPV of all columns in the cost DataFrame apart from the grouping columns.
-
     Args:
         df_cost: contains columns with cost data and "lifetime" and "wacc"
         grouping_cols: list of column headers for grouping of the cost data
-
     Returns:
         pd.DataFrame: columns with NPV of various cost components
     """
     # Calculate NPV over data groups with cost series across model time horizon
     logger.info("Calculate NPV")
-    df_cost = df_cost.drop_duplicates()
+    #! Debug only
+    df_cost = df_cost.loc[df_cost["carbon_cost_addition"] > 0]
     return df_cost.groupby(grouping_cols).apply(calculate_npv_costs)
 
 
 def calculate_npv_costs(df_cost: pd.DataFrame) -> pd.DataFrame:
     """Calculate net present value (NPV) of all cost columns in the DataFrame of costs.
-
     Args:
         df_cost: DataFrame indexed by product, technology_origin, technology_destination, region, type and year
-
     Returns:
         pd.DataFrame: NPV of each column in the DataFrame, indexed by year
     """
 
     df_cost = df_cost.set_index("year")
-    logger.debug(
-        f"Calculate NPV with WACC tech origin {df_cost['technology_origin'].unique()}, tech destination {df_cost['technology_destination'].unique()}"
-    )
     return df_cost.apply(
         lambda row: net_present_value(
             rate=row["wacc"],
@@ -64,6 +58,7 @@ def net_present_value(
     Returns:
         pd.Series: NPVs of the cost columns, indexed by column name
     """
+
     value_share = (1 + rate) ** np.arange(0, len(df))
     if cols is None:
         cols = df.columns
@@ -74,12 +69,10 @@ def subset_cost_df(
     df_cost: pd.DataFrame, start_year: int, lifetime: int
 ) -> pd.DataFrame:
     """Filter DataFrame with cost data for a year range and expand with constant value of the last year if necessary.
-
     Args:
         df_cost (pd.DataFrame): contains columns with cost data, indexed by year (int)
         start_year (int): first year of year range filtering
         lifetime (int): required length of the cost series
-
     Returns:
         pd.DataFrame: subset DataFrame, expanded if necessary
     """
@@ -89,31 +82,20 @@ def subset_cost_df(
     ]
 
     # Expand DataFrame beyond model years if necessary, assuming that cost data stay constant after MODEL_END_YEAR
-    if start_year + lifetime > END_YEAR:
+    if start_year + lifetime > df_cost.index.max():
 
         # TODO: make this workaround nicer
-        cost_value = df_cost.loc[df_cost.index == END_YEAR].copy()
-        cost_value = cost_value.drop_duplicates()
-        extension_length = int((start_year + lifetime) - END_YEAR)
-        cost_constant = pd.concat([cost_value] * extension_length)
-        cost_constant["year"] = np.arange(END_YEAR + 1, start_year + lifetime + 1)
-        cost_constant = cost_constant.set_index("year", drop=True)
-        cost_constant.index = cost_constant.index.astype(int)
-
-        df_cost = pd.concat([df_cost, cost_constant])
-    if start_year + lifetime > df_cost.index.max():
-        #     # TODO: make this workaround nicer
         cost_value = df_cost.loc[
             df_cost.index == df_cost.index.max(), ["carbon_cost_addition"]
         ]
         extension_length = int((start_year + lifetime) - df_cost.index.max())
         cost_constant = pd.concat([cost_value] * extension_length)
-        logger.debug(cost_constant)
-        logger.debug(np.arange(df_cost.index.max() + 1, start_year + lifetime + 1))
         cost_constant["year"] = np.arange(
             df_cost.index.max() + 1, start_year + lifetime + 1
         )
         cost_constant = cost_constant.set_index("year", drop=True)
         cost_constant.index = cost_constant.index.astype(int)
+
         df_cost = pd.concat([df_cost, cost_constant])
+
     return df_cost
