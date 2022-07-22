345972769cc7d2a4cdaea2a2a9ddc1a9743375df
Luis Natera <luis.natera@systemiq.earth>
2022-07-22 15:53:47 +0200

    Minor fix to rank for LC

--- mppshared/solver/ranking.py
+++ mppshared/solver/ranking.py
@@ -107,6 +107,7 @@ def rank_technology_histogram(
             f"{cost_metric}_adjusted_by_emissions_normalized"
         ]
         df_rank = df
+        logger.debug("Calculating final rank")
         df_rank["rank"] = df_rank[f"{rank_type}_{pathway}_score"].rank(ascending=True)
 
     else:
@@ -120,9 +121,9 @@ def rank_technology_histogram(
             _add_binned_rankings, rank_type, pathway, n_bins
         )
 
-    # Calculate final rank for the transition type
-    logger.debug("Calculating final rank")
-    df_rank["rank"] = df_rank[f"{rank_type}_{pathway}_score"].rank(ascending=False)
+        # Calculate final rank for the transition type
+        logger.debug("Calculating final rank")
+        df_rank["rank"] = df_rank[f"{rank_type}_{pathway}_score"].rank(ascending=False)
 
     return df_rank
 
