3fdc25782792714bda972288e8caaac4f42a6af7
Luis Natera <luis.natera@systemiq.earth>
2022-07-25 10:03:13 +0200

    Add maximum_global_demand_share parameter to pathway

--- mppshared/models/simulation_pathway.py
+++ mppshared/models/simulation_pathway.py
@@ -53,6 +53,7 @@ class SimulationPathway:
         constraints_to_apply: list,
         carbon_cost_trajectory: CarbonCostTrajectory = None,
         technologies_maximum_global_demand_share: list = None,
+        maximum_global_demand_share: dict = None,
     ):
         # Attributes describing the pathway
         self.start_year = start_year
@@ -74,6 +75,7 @@ class SimulationPathway:
         self.technologies_maximum_global_demand_share = (
             technologies_maximum_global_demand_share
         )
+        self.maximum_global_demand_share = maximum_global_demand_share
 
         # Carbon Budget (already initialized with emissions pathway)
         self.carbon_budget = carbon_budget
