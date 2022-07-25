1fb64a45040b8b70299e78dd830ca158430d2800
Luis Natera <luis.natera@systemiq.earth>
2022-07-25 14:48:32 +0200

    Change pathway to pathway_name

--- aluminium/solver/ranking.py
+++ aluminium/solver/ranking.py
@@ -1,23 +1,18 @@
 """Create ranking of technology switches (decommission, brownfield, greenfield)."""
 
+from aluminium.config_aluminium import (EMISSION_SCOPES_RANKING, GHGS_RANKING,
+                                        NUMBER_OF_BINS_RANKING, PRODUCTS,
+                                        RANK_TYPES, RANKING_CONFIG,
+                                        RANKING_COST_METRIC)
 from mppshared.import_data.intermediate_data import IntermediateDataImporter
 from mppshared.solver.ranking import rank_technology_histogram
-from aluminium.config_aluminium import (
-    PRODUCTS,
-    RANK_TYPES,
-    RANKING_COST_METRIC,
-    NUMBER_OF_BINS_RANKING,
-    EMISSION_SCOPES_RANKING,
-    GHGS_RANKING,
-    RANKING_CONFIG,
-)
 
 
-def make_rankings(pathway: str, sensitivity: str, sector: str):
+def make_rankings(pathway_name: str, sensitivity: str, sector: str):
     """Create the ranking for the three types of technology switches"""
 
     importer = IntermediateDataImporter(
-        pathway=pathway,
+        pathway=pathway_name,
         sensitivity=sensitivity,
         sector=sector,
         products=PRODUCTS,
@@ -29,10 +24,10 @@ def make_rankings(pathway: str, sensitivity: str, sector: str):
         df_rank = rank_technology_histogram(
             df_ranking=df_ranking,
             rank_type=rank_type,
-            pathway=pathway,
+            pathway=pathway_name,
             cost_metric=RANKING_COST_METRIC,
             n_bins=NUMBER_OF_BINS_RANKING,
-            ranking_config=RANKING_CONFIG[rank_type][pathway],
+            ranking_config=RANKING_CONFIG[rank_type][pathway_name],
             emission_scopes_ranking=EMISSION_SCOPES_RANKING,
             ghgs_ranking=GHGS_RANKING,
         )
