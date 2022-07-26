3fca659150657cfa4cbd4fb617b48bfcf63712d1
Luis Natera <luis.natera@systemiq.earth>
2022-07-26 19:04:23 +0200

    Minor updates to naming

--- cement/solver/ranking.py
+++ cement/solver/ranking.py
@@ -30,7 +30,7 @@ def make_rankings(pathway_name: str, sensitivity: str, sector: str, products: li
                 pathway_name=pathway_name,
                 cost_metric=RANKING_COST_METRIC,
                 n_bins=NUMBER_OF_BINS_RANKING,
-                ranking_config=RANKING_CONFIG[rank_type][pathway],
+                ranking_config=RANKING_CONFIG[rank_type][pathway_name],
                 emission_scopes_ranking=EMISSION_SCOPES_RANKING,
                 ghgs_ranking=GHGS_RANKING,
             )
@@ -41,7 +41,7 @@ def make_rankings(pathway_name: str, sensitivity: str, sector: str, products: li
                 pathway_name=pathway_name,
                 cost_metric=RANKING_COST_METRIC,
                 cost_metric_relative_uncertainty=COST_METRIC_RELATIVE_UNCERTAINTY,
-                ranking_config=RANKING_CONFIG[rank_type][pathway],
+                ranking_config=RANKING_CONFIG[rank_type][pathway_name],
                 emission_scopes_ranking=EMISSION_SCOPES_RANKING,
                 ghgs_ranking=GHGS_RANKING,
             )
