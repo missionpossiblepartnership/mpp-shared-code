ee19fef81c0e5d5d490bc3d56a6addb181e03fd4
nateraluis <luis.natera@systemiq.earth>
2022-08-15 10:29:18 +0200

    #223 test technology availability

--- tests/test_implicit_forcing.py
+++ tests/test_implicit_forcing.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-from mppshared.solver.implicit_forcing import \
-    add_technology_classification_to_switching_table
+from mppshared.solver.implicit_forcing import (
+    add_technology_classification_to_switching_table,
+    apply_technology_availability_constraint,
+)
 
 
 def test_add_technology_classification_to_switching_table():
@@ -13,3 +15,27 @@ def test_add_technology_classification_to_switching_table():
         df_switching_table, df_technology_characteristics
     )
     assert "technology_classification" in df_technologies.columns
+
+
+def test_apply_technology_availability_constraint():
+    df_switching_table = pd.read_csv("tests/test_data/technology_transitions.csv")
+    df_technology_characteristics = pd.read_csv(
+        "tests/test_data/technology_characteristics.csv"
+    )
+    df_technologies = apply_technology_availability_constraint(
+        df_switching_table, df_technology_characteristics, start_year=2020
+    )
+    assert (
+        df_technologies[
+            df_technologies["technology_destination"] == "Inert Anode + Grid"
+        ]["year"].min()
+        == 2030
+    )
+    assert (
+        len(
+            df_technologies[
+                df_technologies["technology_origin"] == "Inert Anode + Grid"
+            ]
+        )
+        == 0
+    )
