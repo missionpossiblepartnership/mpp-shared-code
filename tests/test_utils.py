33ba251a29eb85a39f981c6b01e7176b7153172e
nateraluis <luis.natera@systemiq.earth>
2022-08-23 14:04:09 +0200

    Fix failing test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,4 +8,4 @@ from mppshared.utility.dataframe_utility import move_cols_to_front
 def test_move_cols_to_front():
     test_df = pd.DataFrame([(1, 2, 3), (4, 5, 6)], columns=["A", "B", "C"])
     mod_cols = move_cols_to_front(test_df, ["C"])
-    assert mod_cols == ["C", "A", "B"]
+    assert mod_cols[0] == "C"
