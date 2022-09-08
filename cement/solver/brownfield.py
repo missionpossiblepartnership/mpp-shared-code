1cf5df36d52a1928a627417f821f748ce3887821
timon131 <tiwerb@hotmail.de>
2022-09-08 16:47:55 +0200

    fix bio constraint bug

--- cement/solver/brownfield.py
+++ cement/solver/brownfield.py
@@ -328,9 +328,9 @@ def _enact_brownfield_transition(
                         ],
                     )
 
-            # ALTERNATIVE FUEL
-            if "alternative_fuel_constraint" in constraints_to_apply:
-                if not dict_constraints["alternative_fuel_constraint"]:
+            # BIOMASS
+            if "biomass_constraint" in constraints_to_apply:
+                if not dict_constraints["biomass_constraint"]:
                     # remove all transitions with that destination technology from the ranking table
                     logger.debug(
                         f"Handle biomass constraint: removing destination technology"
