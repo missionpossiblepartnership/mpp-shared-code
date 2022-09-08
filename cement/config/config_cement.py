fix investments calculation

cement/config/config_cement.py
@@ -27,9 +27,9 @@ END_YEAR = 2050
 MODEL_YEARS = np.arange(START_YEAR, END_YEAR + 1)
 
 PATHWAYS_SENSITIVITIES = {
-    # "bau": ["def"],  # ALL_SENSITIVITIES,
+    "bau": ["def"],  # ALL_SENSITIVITIES,
     # "fa": ["def"],
-    "lc": ["def"],  # ALL_SENSITIVITIES,
+    # "lc": ["def"],  # ALL_SENSITIVITIES,
 }
 
 PATHWAYS_WITH_CARBON_COST = ["lc"]